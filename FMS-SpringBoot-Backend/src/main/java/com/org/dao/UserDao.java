9e391ed8f683c81cebd821ce7056b025060b9c03
Shayan Mondal <shayonmondal@gmail.com>
2021-08-03 14:12:46 +0530

    First commit

--- FMS-SpringBoot-Backend/src/main/java/com/org/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package com.org.dao;
-import java.math.BigInteger;
-
-import org.springframework.data.repository.CrudRepository;
-import com.org.model.Users;;
-
-public interface UserDao extends CrudRepository<Users, BigInteger>{
-
-}
